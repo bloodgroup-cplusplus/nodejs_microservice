5055d7055ef28908f02da7a525a9575bbdba908b
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-09 15:58:52 +0530

    added packages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hollow world")
+const express = require("express")
+
+const app = express()
+
+const port = 3000
+
+app.get("/",(req,res)=>{
+    res.send("heoowl world")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
