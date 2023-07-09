1c1d8d27235f8cf5673fe02657ef0abf568b9791
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-09 16:20:01 +0530

    this contained only the node js part

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require("express")
 
 const app = express()
+const axios = require("axios")
 
 const port = 3000
 
 app.get("/",(req,res)=>{
     res.send("heoowl world")
+})
+
+app.get("/todos",(req,res)=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(
+        response=>{
+            res.json(response.data)
+        }
+    )
+    .catch(err=>{
+        console.log(err);
+        res.statusCode(500);
+    });
+
+
 })
 
 
